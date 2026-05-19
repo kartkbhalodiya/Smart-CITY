4722918d97a088c9cd2ef3018b5b3bf6c1500df8
kartik Bhalodiya <145188187+kartkbhalodiya@users.noreply.github.com>
2026-05-19 12:21:34 +0530

    Add explicit login route

--- complaints/urls.py
+++ complaints/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login_view, name='login'),
+    path('login/', views.login_view, name='login'),
+    path('', views.login_view, name='home'),
     path('forgot-password/', views.forgot_password, name='forgot_password'),
     path('register/', views.register_view, name='register'),
     path('verify-otp/', views.verify_otp_view, name='verify_otp'),
